83d0ca11d33b837e337173c4e6e1a961ffd83ff7
Jarsky <jarskynz@gmail.com>
2022-04-23 09:30:13 +1200

    Update backup-pi.sh

--- backup-pi.sh
+++ backup-pi.sh
@@ -14,6 +14,8 @@ hostname=$(hostname)
 logfile="/var/log/backup-pi.log"
 backup_path="/mnt/backup"
 threshold=$(date -d "15 days ago" +%d%m%y)
+dependencyCheck=true
+enableLogging=true
 
 #Email Settings
 #using sendEmail e.g apt install sendemail
@@ -28,10 +30,54 @@ servername="My Server Name"
 backupemailTemplate=emails/backup.eml
 emailTemp=emails/backup.tmp
 
+#Console Colors
+RED='\e[1;31m'
+YEL='\e[0;33m'
+GRN='\e[0;92m'
+NC='\e[0m'
+OK='\e[0;92m\u2714\e[0m'
+ERR='\e[1;31m\u274c\e[0m'
+
 export emlhostname=$hostname
 export emlFromName=$fromName
 
+# Logging
+if [ $enableLogging = "true" ]; then
 exec 1>> >(ts '[%Y-%m-%d %H:%M:%S]' >> "$logfile") 2>&1
+fi
+
+# Check Dependencies
+if [ $dependencyCheck = "true" ]; then
+        if [ -f /etc/lsb-release ]; then
+            . /etc/lsb-release
+                OS=$DISTRIB_ID
+                VER=$DISTRIB_RELEASE
+        elif [ -f /etc/debian_version ]; then
+                OS=Debian
+                VER=$(cat /etc/debian_version)
+        elif [ -f /etc/redhat-release ]; then
+                OS=CentOS
+                VER=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | cut -d"-" -f3)
+        else
+                OS=$(uname -s)
+                VER=$(uname -r)
+        fi
+
+        if [ $OS = "Ubuntu" ] && [ $(dpkg-query -W -f='${Status}' sendemail 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+                        apt-get -y install sendemail
+                        echo ""
+                        echo -e "${GRN}Package has been installed, you can now run ${BASH_SOURCE[0]}${NC}\n"
+                        echo ""
+                        exit 0
+
+        elif [ $OS = "CentOS" ] && [ yum -q list installed moreutils &>/dev/null && echo "Error" ]; then
+                        yum -y install sendemail
+                        echo ""
+                        echo -e "${GRN}Package has been installed, you can now run ${BASH_SOURCE[0]}${NC}\n"
+                        echo ""
+                        exit 0
+        fi
+fi
 
 check_file="$backup_path/check"
 if [ ! -f "$check_file" ]; then
